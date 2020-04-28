6aca1fbde8e82b3dc242eaf2a350db9271ca1019
qqwei <57345553+qqroots@users.noreply.github.com>
2020-04-28 11:53:58 +0800

    將專案內容上傳

---
+++ shop/order/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Order(models.Model):
+    order_user = models.DecimalField(max_digits = 10, decimal_places = 0) #查user id 用__set看看
+    # order_amount = models.DecimalField(max_digits=6,decimal_places=0)
+    order_total = models.DecimalField(max_digits=6,decimal_places=0)   
+
+class OrderItem(models.Model):
+    product_name = models.CharField(max_length = 50)
+    product_price = models.DecimalField(max_digits = 3, decimal_places = 0)
+    product_qty = models.IntegerField()
+    order = models.ForeignKey(Order,on_delete=models.CASCADE)
---
+++ shop/shop/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render
+from django import http
+from product.models import Product
+from order.models import OrderItem,Order
+from django.http import HttpResponseRedirect,HttpResponse,Http404
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth
+from django.urls import reverse
+
+def base(request):
+    return render(request,'base.html')
+
+# def logout(request):
+#     #to do 
+#     return HttpResponseRedirect('/')
+
+def index(request):
+    return render(request,'index.html',)
+
+def get_shop(request):
+    products = Product.objects.all()
+    return render(request,'shop.html',locals())
+
+def get_order(request):
+    if 'id' in request.GET and request.GET['id'] != '':
+        ids = request.GET.getlist('id',[])           #html有勾選的項目取出其產品id
+        qtys = request.GET.getlist('product_qty',[]) #html有勾選的項目取出其數量
+        content = dict(zip(ids,qtys))
+    else:
+        raise Http404
+
+    order_items = []
+    total = 0
+    # for i in content:
+    #     p = Product.objects.get(id=i)
+    #     order_item = [p.product_name,p.product_price,content[i],p.pk]
+    #     order_items.append(order_item)
+    #     total += int(order_item[1]) * int(order_item[2])
+    for i in content:
+        p = Product.objects.get(id=i)
+        order_items.append([p.pk,p.product_name,p.product_price,content[i]])
+        total += int(p.product_price) * int(content[i])
+
+    return render(request,'order_item.html',locals())
+
+@login_required
+def order_confrim(request):
+    if request.user:
+        user_id = request.user.pk
+        user_name = request.user.username
+
+    ids = request.POST.getlist('product_pk',[])   #html有勾選的項目取出其產品id
+    qtys = request.POST.getlist('product_qty',[]) #html有勾選的項目取出其數量
+    content = dict(zip(ids,qtys))
+    total = request.POST["total"]
+    
+    order = Order.objects.create(order_user=request.user.pk ,order_total=total) #先創造一筆空的Order物件
+
+    order_items = []
+    for i in content:
+        p = Product.objects.get(id=i)
+        order_item = OrderItem.objects.create(
+            product_name=p.product_name, product_price=p.product_price, product_qty=content[i],order=order
+            )
+        order_items.append(order_item)
+    return render(request,'order_confirm.html',locals())
